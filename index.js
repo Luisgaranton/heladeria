ac08d53c08278bf56d3af5b87cc60375d40d524a
luis <luisgaranton04@gmail.com>
2022-09-20 15:12:07 -0400

    agg y borramos de sabores del arreglo

--- index.js
+++ index.js
@@ -6,21 +6,36 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var sabores = [];
+var basededatos_sabores = [];//arreglo de sabores
 
 
 app.get('/sabores/lista', (req, res) => {
-  res.json(sabores);
+  res.json(basededatos_sabores);
 })
 
 app.post('/sabores', (req, res) => {
   let sabor = req.body.sabor
-  sabores.push(sabor);
+  basededatos_sabores.push(sabor);
   res.json({"mensaje": 'se guardo en la base de datos'})
 })
 
 app.delete('/sabores', (req, res) => {
-  res.send('guardar sabores');
+  console.log("req body", req.body);
+  let sabor = req.body.sabor
+  console.log("variable sabor", sabor);
+  var mensaje = "se borro el sabor especificado";
+  if (basededatos_sabores.length >= 1){
+    var idx = basededatos_sabores.indexOf(sabor);
+    console.log("indice", idx);
+    if(idx != -1) {
+      basededatos_sabores = basededatos_sabores.slice(idx-1, idx)
+    }else{
+      mensaje = " no se encontro la info";
+    }
+  }else {
+    mensaje = " no se encontro la info";
+  }
+  res.json({"mensaje": mensaje});
 })
 
 app.put('/sabores', (req, res) => {
